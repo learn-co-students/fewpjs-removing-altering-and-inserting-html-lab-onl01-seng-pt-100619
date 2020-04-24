3baf80c303c104be78f36fe013454574b19b12ba
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-24 12:41:36 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+let element = document.querySelector("main#main");
+
+element.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+document.body.appendChild(newHeader);
+
+// let newHeader = document.querySelector("h1#victory");
+
+let text = document.createTextNode("YOUR-NAME is the champion");
+
+newHeader.appendChild(text);
