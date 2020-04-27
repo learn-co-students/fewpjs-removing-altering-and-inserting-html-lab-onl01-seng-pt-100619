39f64e9f1c9442d168cc7105d7e8ba36e08ee469
github username <github email address>
2020-04-26 23:23:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let main = document.querySelector('main')
 main.remove()
 
+// my solution
 let newHeader = document.createElement('h1')
-// let victory = document.getElementById('victory')
-// document.body.insertBefore(newHeader, victory)
 newHeader.setAttribute("id","victory")
 newHeader.innerHTML = "POUYA is the champion"
+
+// Flatiron solution
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
