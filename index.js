62f784a676b8fcc40b50cddbde910eb098818bec
github username <github email address>
2020-04-26 23:19:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.querySelector('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+// let victory = document.getElementById('victory')
+// document.body.insertBefore(newHeader, victory)
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "POUYA is the champion"
