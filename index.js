f37b52c3375b3780efd871c933d1a06247db4408
janewysoki <jane.wysoki@gmail.com>
2020-05-05 12:48:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let elementMain = document.querySelector("main#main");
+elementMain.remove();
+
+
+var newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Jane is the champion";
