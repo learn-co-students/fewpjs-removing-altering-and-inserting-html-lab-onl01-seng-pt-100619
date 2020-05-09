ca9c81902b6ffc6a195b14e55b73699371a9467a
anikachristopher <anika.mchristoper@gmail.com>
2020-05-08 22:37:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector("main");
+    document.body.removeChild(main);
+    
+let newHeader = document.createElement("h1");
+    newHeader.id = "victory";
+
+newHeader.innerHTML = "Anika is the champion";
